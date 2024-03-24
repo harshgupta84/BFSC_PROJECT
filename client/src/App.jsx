aeca7b44cf85410340e9614ae4097fa8334ebd8b
harshleet <harshguptalnm@gmail.com>
2024-03-24 21:06:33 +0530

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" bg-slate-500">
+      <h1 className=" text-blue-500">Harsh</h1>
+    </div>
+  );
+};
+
+export default App;
