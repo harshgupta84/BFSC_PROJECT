6800c9b093bab794fbf5bf3b54f25430029546f9
harzh01 <harzh.0110@gmail.com>
2024-04-04 09:09:15 +0530

    harzhchanges

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,36 +1,31 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { logo, sun } from '../assets';
+import { sun } from '../assets';
 import { navlinks } from '../constants';
 
-const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
-    {!isActive ? (
-      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
-    ) : (
-      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
-    )}
+const Icon = ({ name, isActive, disabled, handleClick }) => (
+  <div className={`w-[95px] h-[48px] rounded-[10px] bg-[#25384a] flex justify-center items-center cursor-pointer font-bold text-white ${isActive && isActive === name ? 'bg-[#2c2f32]' : ''}`} onClick={handleClick}>
+    {name}
   </div>
-)
+);
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
 
   return (
-    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-      </Link>
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[9vh]">
+      
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3">
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#030303] rounded-[20px] w-[110px] py-4 mt-12">
+        <div className=" font-epilogue flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
             <Icon 
               key={link.name}
-              {...link}
+              name={link.name}
               isActive={isActive}
+              disabled={link.disabled}
               handleClick={() => {
                 if(!link.disabled) {
                   setIsActive(link.name);
@@ -41,10 +36,11 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        {/* Display sun icon */}
+        
       </div>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
